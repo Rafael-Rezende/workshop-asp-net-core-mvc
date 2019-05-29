277d039978f9e55866e418085f5b302ca4ad7254
Rafael Rezende <rafaeldeabreurezende@hotmail.com>
2019-05-28 22:32:25 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,17 +8,29 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {1} and {2}")]
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         public string Email { get; set; }
-        [Display(Name = "Birth Date")]
+
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "Birth Date")]
+        [Required(ErrorMessage = "{0} required")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM0/yyyy}")]
         public DateTime BirthDate { get; set; }
+
         [Display(Name = "Base Salary")]
+        [Required(ErrorMessage = "{0} required")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
